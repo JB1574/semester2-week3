ea111c341b718ab39b843589cddf8e8465e09f64
JB1574 <mrln0367@leeds.ac.uk>
2026-02-23 18:44:45 +0000

    start of portfolio task

--- worksheet/portfolio/salary.c
+++ worksheet/portfolio/salary.c
@@ -6,13 +6,16 @@
  * Name:
  * ID:
  */
-
+#include <stdio.h>
  int main( void ) {
 
     // define and initialise variables for the problem data 
+   float salary = 36250; 
+   float NI = 8;
+   float Tax_rate = 15;
 
     // calculate the deductions and final take-home salary
-
+   float NI_contribution = 
     // Use only these print statement with appropriate formatting and variable names
     //printf("Salary £...",var_name);
     //printf("NI contribution £...",var_name);
